266a285c7f2e2af2d46eadcfb55776c771db911d
SuperJoy <superjoy0502@gmail.com>
2021-07-23 01:07:12 -0600

    🧛‍♂️ 기본 세팅 2

--- src/main/kotlin/io/github/superjoy0502/socialdistancing/SocialDistancingPlugin.kt
+++ src/main/kotlin/io/github/superjoy0502/socialdistancing/SocialDistancingPlugin.kt
@@ -1,14 +1,26 @@
-package io.github.monun.sample.plugin
+package io.github.superjoy0502.socialdistancing
 
 import io.github.monun.kommand.kommand
 import net.kyori.adventure.text.Component.text
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
- * @author Monun
+ * @author superjoy0502
  */
-class SamplePlugin : JavaPlugin() {
+
+class SocialDistancingPlugin : JavaPlugin() {
+
     override fun onEnable() {
+        setupCommands()
+        logger.info(ChatColor.GREEN.toString()  + "플러그인 활성화")
+    }
+
+    override fun onDisable() {
+        logger.info(ChatColor.RED.toString() + "플러그인 비활성화")
+    }
+
+    private fun setupCommands() {
         kommand {
             register("sample") {
                 then("ping") {
